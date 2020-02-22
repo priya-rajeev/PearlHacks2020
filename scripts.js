f4900ca42a7942ca332bcfbe0091df7a29ef8f0e
priya-rajeev <54964012+priya-rajeev@users.noreply.github.com>
2020-02-21 23:37:37 -0500

    Test
    Just testing

---
+++ scripts.js
@@ -0,0 +1,80 @@
+{/*<script src="https://api.mqcdn.com/sdk/mapquest-js/v1.3.2/mapquest.js"></script>*/}
+{/*    <link type="text/css" rel="stylesheet" href="https://api.mqcdn.com/sdk/mapquest-js/v1.3.2/mapquest.css"/>*/}
+
+{/*    let L;*/}
+
+{/*let map = L.mapquest.map('map', {*/}
+{/*    center: [53.480759, -2.242631],*/}
+{/*    layers: L.mapquest.tileLayer('map'),*/}
+{/*    zoom: 12*/}
+{/*});*/}
+
+
+// Replace ./data.json with your JSON feed
+fetch('https://ghibliapi.herokuapp.com/films')
+    .then(response => {
+        return response.json()
+    })
+    .then(data => {
+        // Work with JSON data here
+
+        // var data1 = JSON.parse(this.response)
+
+        const app = document.getElementById('root')
+
+        const logo = document.createElement('img')
+        logo.src = 'logo.png'
+
+        const container = document.createElement('div')
+        container.setAttribute('class', 'container')
+
+        app.appendChild(logo)
+        app.appendChild(container)
+
+
+        data.forEach(movie => {
+            // Create a div with a card class
+            const card = document.createElement('div')
+            card.setAttribute('class', 'card')
+
+            // Create an h1 and set the text content to the film's title
+            const h1 = document.createElement('h1')
+            h1.textContent = movie.title
+
+            // Create a p and set the text content to the film's description
+            const p = document.createElement('p')
+            movie.description = movie.description.substring(0, 300) // Limit to 300 chars
+            p.textContent = `${movie.description}...` // End with an ellipses
+
+            // Append the cards to the container element
+            container.appendChild(card)
+
+            // Each card will contain an h1 and a p
+            card.appendChild(h1)
+            card.appendChild(p)
+        })
+    })
+    .catch(err => {
+        // Do something for an error here
+        throw new SQLException()
+    })
+
+
+// // Create a request variable and assign a new XMLHttpRequest object to it.   //old
+// var request = new XMLHttpRequest()
+//
+// // Open a new connection, using the GET request on the URL endpoint
+// request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
+//
+// request.onload = function() {
+//     // Begin accessing JSON data here
+// }
+//
+// // Send request
+// request.send()
+
+
+
+// Begin accessing JSON data here
+
+
